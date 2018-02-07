d124fa17c629063481c2cb5a44304571eb32fbcd
Frank Hettrich <hetty@hettyfun.eu>
2018-02-07 10:54:02 +0100

    mc also start with the access ssh

--- profile.d/hm-tools-addons.sh
+++ profile.d/hm-tools-addons.sh
@@ -1,2 +1,3 @@
 export PATH=${PATH}:/usr/local/addons/hm-tools/bin
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/addons/hm-tools/lib:/usr/local/addons/hm-tools/lib/expect5.45.3
+export TERM=xterm
